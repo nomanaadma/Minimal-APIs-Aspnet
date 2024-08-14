20f5531ac0df599df1ef67ffee8d9e2e6f1cf837
Noman Shoukat <nomanaadma@gmail.com>
2024-08-14 16:30:03 +0500

    book crud

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.SymbolStore;
 using FluentValidation;
 using FluentValidation.Results;
 using Library.Api.Data;
@@ -44,6 +45,58 @@ app.MapPost("books", async (Book book,
 	}
 
 	return Results.Created($"/books/{book.Isbn}", book);
+
+});
+
+app.MapGet("books", async (IBookService bookService, string? searchTerm) =>
+{
+	
+	IEnumerable<Book> books;
+	
+	if (searchTerm is not null && !string.IsNullOrEmpty(searchTerm))
+		books = await bookService.SearchByTitleAsync(searchTerm);
+	else 
+		books = await bookService.GetAllAsync();
+		
+	return Results.Ok(books);
+	
+});
+
+app.MapGet("books/{isbn}", async (string isbn,  IBookService bookService) =>
+{
+	var book = await bookService.GetByIsbnAsync(isbn);
+	return book is not null ?  Results.Ok(book) : Results.NotFound();
+});
+
+
+app.MapPut("books/{isbn}", async (string isbn, Book book, 
+	IBookService bookService,
+	IValidator<Book> validator
+) =>
+{
+
+	book.Isbn = isbn;
+
+	var validationResult = await validator.ValidateAsync(book);
+	if (!validationResult.IsValid)
+	{
+		return Results.BadRequest(validationResult.Errors);
+	}
+
+	var updated = await bookService.UpdateAsync(book);
+
+	return updated ? Results.Ok(book) : Results.NotFound() ;
+
+});
+
+
+app.MapDelete("books/{isbn}", async (string isbn, 
+	IBookService bookService
+) =>
+{
+	
+	var deleted = await bookService.DeleteAsync(isbn);
+	return deleted ? Results.NoContent() : Results.NotFound() ;
 
 });
 
--- Services/BookService.cs
+++ Services/BookService.cs
@@ -30,26 +30,52 @@ public class BookService : IBookService
 
 	public async Task<Book?> GetByIsbnAsync(string isbn)
 	{
-		throw new NotImplementedException();
+		using var connection = await _connectionFactory.CreateConnectionAsync();
+
+		return await connection.QuerySingleOrDefaultAsync<Book>(
+			"SELECT * FROM books WHERE Isbn = @Isbn LIMIT 1", new { Isbn = isbn });
+
 	}
 
 	public async Task<IEnumerable<Book>> GetAllAsync()
 	{
-		throw new NotImplementedException();
+		using var connection = await _connectionFactory.CreateConnectionAsync();
+		return await connection.QueryAsync<Book>("SELECT * FROM books");
 	}
 
 	public async Task<IEnumerable<Book>> SearchByTitleAsync(string searchTerm)
 	{
-		throw new NotImplementedException();
+		using var connection = await _connectionFactory.CreateConnectionAsync();
+		return await connection.QueryAsync<Book>(
+			"SELECT * FROM books WHERE title LIKE '%' || @SearchTerm || '%'", new { SearchTerm = searchTerm });
+
 	}
 
 	public async Task<bool> UpdateAsync(Book book)
 	{
-		throw new NotImplementedException();
+		var existingBook = await GetByIsbnAsync(book.Isbn);
+
+		if (existingBook is null)
+		{
+			return false;
+		}
+		
+		using var connection = await _connectionFactory.CreateConnectionAsync();
+		var result = await connection.ExecuteAsync(
+			"""
+			UPDATE Books SET Title = @Title, Author = @Author, ShortDescription = @ShortDescription,
+			PageCount = @PageCount, ReleaseDate = @ReleaseDate WHERE Isbn = @Isbn
+			""",
+			book);
+		return result > 0;
+		
 	}
 
 	public async Task<bool> DeleteAsync(string isbn)
 	{
-		throw new NotImplementedException();
+		using var connection = await _connectionFactory.CreateConnectionAsync();
+		var result = await connection.ExecuteAsync(
+			"DELETE FROM Books WHERE Isbn = @Isbn", new {Isbn = isbn});
+		return result > 0;
 	}
 }
